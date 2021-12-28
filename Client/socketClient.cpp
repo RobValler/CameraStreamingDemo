5904620553d173c1e9c11afb5f984f735c6a03b4
R V <r.valler@yahoo.co.uk>
2021-12-28 21:50:23 +0100

    Server and Client have been updated
    Connection established using Comms libs but
    there are till issues with read and write.

--- Client/socketClient.cpp
+++ Client/socketClient.cpp
@@ -9,12 +9,11 @@
 
 #include <stdio.h>	//printf
 #include <string.h>	//strlen
-#include <sys/socket.h>	//socket
-#include <arpa/inet.h>	//inet_addr
-#include <unistd.h> // close socket
 
-#include "mainWindow.h"
 #include "socketClient.h"
+#include "mainWindow.h"
+#include "comms/comm_client.h"
+#include "example.pb.h"
 
 socketClient::socketClient(MainWindow* ref): refMainWnd(ref)
 {
@@ -34,82 +33,31 @@ socketClient::~socketClient()
 
 void socketClient::f_socketRead()
 {
-    int read_count;
+    CCommClient sock_client(client_proto::ETCTPIP);
+    camera_msg in;
+    std::uint32_t size;
 
-    int sock;
-    struct sockaddr_in server;
-
-    //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
-
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    //server.sin_addr.s_addr = inet_addr("192.168.178.36");  // WiFi
-    //server.sin_addr.s_addr = inet_addr("192.168.178.21");   // Ethernet
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 8888 );
-
-    //Connect to remote server
-    if (connect(sock, (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("connect failed. Error");
+    if(!sock_client.connect("127.0.0.1"))
         return;
-    }
 
-    puts("Connected\n");
+    std::uint32_t index = 0;
 
-
-    // handle the handshake
-    if( recv(sock , &HS , sizeof(handshake) , 0) < 0)
-    {
-        puts("recv failed");
-        return;
-    }
-
-    if(HS.ImgBufferSize <= 0)
-    {
-        puts("Handshake failed");
-        return;
-    }
-
-    printf("buffer size = %d\n", HS.ImgBufferSize);
-
-    if( send(sock , &HS , sizeof(handshake) , 0) < 0)
-    {
-        puts("Send failed");
-        return;
-    }
-
-    // pre loop init
-    read_count = 0;
-    imageDataBuffer.resize(HS.ImgBufferSize);
-    refMainWnd->API_Init_ImageBuffer(HS.ImgBufferSize);
-
-
-    //keep communicating with server
     while(true)
     {
         if( exit_request == true)
             break;
 
-        // received image data from the socket
-        read_count = recv(sock, &imageDataBuffer[0], HS.ImgBufferSize, MSG_WAITALL);
+        if(!sock_client.read(&in))
+            break;
 
-        if( read_count < 0) {
-            puts("receive failed");
-            //break;
-        } else if( read_count == HS.ImgBufferSize ) {
-            read_count = 0;
-
-            //refMainWnd->update((void*) &imageDataBuffer[0], (size_t) HS.ImgBufferSize);
-            updateFlag = true;
+        size = in.data_size();
+        imageDataBuffer = {};
+        for(index = 0; index < size; ++index) {
+            imageDataBuffer.at(index) = in.data(index);
         }
+        if(index == size)
+            updateFlag = true;
     }
-    close(sock);
 }
 
 void socketClient::f_imageForward()
@@ -127,5 +75,3 @@ void socketClient::f_imageForward()
         }
     }
 }
-
-
--- Server/main.cpp
+++ Server/main.cpp
@@ -13,24 +13,78 @@ extern "C"
 #endif
 
 #include "v4l2.h"
-#include<stdio.h>
-#include "server.h"
+#include <stdio.h>
+#include <signal.h>
+//#include "server.h"
 
 #ifdef __cplusplus
 }
 #endif
 
-#include "example.pb.h"
+#include <vector>
+#include <iostream>
+#include <atomic>
 
-int main(int argc, char *argv[])
+#include "example.pb.h"
+#include "comms/comm_server.h"
+
+
+namespace {
+    std::atomic<bool> isInterrupted;
+}
+
+void my_handler(int)
 {
+    isInterrupted = true;
+    std::cout << "program aborted" << std::endl;
+}
+
+//int main(int argc, char *argv[])
+int main()
+{      
+    CCommServer server(server_proto::ETCTPIP);
+    camera_msg out;
+    std::vector<char> message;
+    std::uint32_t size = 0;
+    std::uint32_t messages_sent = 0;
+
+    // interrupt handler
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = my_handler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL); // CTRL+C
+    isInterrupted = false;
+
+
 	init_v4l2();
-	socket_init();
+    //socket_init();
+
+    while(!isInterrupted)
+    {
+        size = v4l2_get_image_size();
+        message.resize(size);
+        v4l2_get_image(&message[0]);
+
+        for(std::uint32_t index = 0; index < size; ++index) {
+            out.add_data(message.at(index));
+        }
+
+        if(!server.write(&out)) {
+            std::cout << "messages was not sent = " << messages_sent << std::endl;
+            continue;
+        }
+
+        out.clear_data();
+        messages_sent++;
+    }
+
+    std::cout << "messages sent = " << messages_sent << std::endl;
 
     /* The program exits after a keypress*/
-    getchar();
+   // getchar();
 
-	socket_exit();
+    //socket_exit();
 	exit_v4l2();
 
 	return 0;
